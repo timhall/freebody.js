318c23378a4ed663a2c7f49adfff0cef5e498b06
Tim Hall <tim.hall.engr@gmail.com>
2013-04-06 20:25:13 -0400

    Add build container

---
+++ src/build/freebody.js
@@ -0,0 +1,13 @@
+var freebody = (function(global){
+    "use strict";
+
+    // Define and export the freebody namespace
+    var freebody = {};
+
+// @include ../utils.js
+// @include ../Vector.js
+// @include ../Body.js
+// @include ../gravity.js
+
+    return freebody;
+})(this);
